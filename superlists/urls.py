aed4161f2f91b0289f815b228d917fa99755dd26
brookman1 <ystreltsov_99@yahoo.com>
2025-06-26 12:12:11 -0400

    First unit and url mapping, dummy view

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+#from django.contrib import admin
+from django.urls import path, re_path
+import lists.views as views
+# from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    re_path(r'^$', views.home_page, name='home'),
 ]
